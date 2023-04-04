9fd77c5fae55e0d0815b3b3046b0ac1f084ad878
Ahasanul Hoque <109936616+hoquecse@users.noreply.github.com>
2023-04-04 20:50:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// var number1=21;
+// var number2=20;
+// console.log(number1 + number2);
+// var num1=40;
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+var num=0;
+while(num<=10){
+    console.log(num);
+    num++
+}
